f7fc241303b5863f58580a6171b6b7ff06842247
sadaharu76 <98842170+sadaharu76@users.noreply.github.com>
2023-08-13 17:51:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+for(var i=0;i<7;i++)
+{
+document.getElementsByClassName("drum")[i].addEventListener("click",function()
+{
+    var so= this.innerHTML;
+    switch(so)
+    {
+        case 'w':
+            var audio= new Audio('sounds/tom-1.mp3');
+            audio.play();
+        case 'a':
+            var audio= new Audio('sounds/tom-2.mp3');
+            audio.play();
+            default: var audio= new Audio('sounds/crash.mp3');
+            audio.play();
+    }
+
+   ani(so);
+});
+}
+document.addEventListener("keypress",function(ke) //ke allows us to tap into the event which trigerrs the event listener 
+{
+    switch(ke.key) //key will check the particular key w/s required
+    {
+        case 'w':
+            var audio= new Audio('sounds/tom-1.mp3');
+            audio.play();
+        case 'a':
+            var audio= new Audio('sounds/tom-2.mp3');
+            audio.play();
+        default: var audio= new Audio('sounds/kick-bass.mp3');
+            audio.play();
+    }
+    ani(ke.key);
+})
+function ani(curr)
+{
+    var activeb= document.querySelector("."+curr); //.curr is how you access classes like .drum
+    activeb.classList.add("pressed");
+    setTimeout(function(){
+        activeb.classList.remove("pressed");
+    }),100; //100 means 0.1 sec
+}
